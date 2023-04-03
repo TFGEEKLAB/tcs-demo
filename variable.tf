f2c83dacf30a2deb1107324bd44eaa7a08ffccc8
Sukumar Sankaran <SukumarSankaran@Sukumar.lan>
2023-04-03 12:57:17 -0400

    Initial commit

---
+++ variable.tf
@@ -0,0 +1,9 @@
+variable "aws_access_key" {
+default = “AKIATKVJB75CNZAZKJCS”
+}
+variable "aws_secret_key" {
+default = “mjbBM06vtAKKqoCrOOYyJCK1xsvak1OBS7aL01fO”
+ }
+variable "region" {
+    default = "us-east-2"
+}
